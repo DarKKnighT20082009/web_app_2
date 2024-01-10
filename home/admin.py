b6a5477654fbaada295ff683c7419a3331d125ab
kamron <kamrontoxirov001@gmail.com>
2024-01-10 17:56:24 +0500

    second commit

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from home.models import Vazifa
+
 # Register your models here.
+
+admin.site.register(Vazifa)
+
+# admin, 2928
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 
 from django.http import HttpResponse
 
+from home.models import Vazifa
+
 
 def home_page(request):
     return HttpResponse("Hello World! Komron")
